1c621b97d2c821383636a00986e40dfe1c354701
Юрий <yurchenkoWork@yandex.ru>
2025-09-10 14:29:37 +0500

    Реализовать основной компонент блога с Routes

---
+++ src/blog.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+const Content = styled.div`
+  padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+  text-align: center;
+`;
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
+
+function Blog() {
+  return (
+    <>
+      <Header />
+      <Content>
+        <H2>Content страницы</H2>
+        <Routes>
+          <Route path="/" element={<div>Главная</div>} />
+          <Route path="/login" element={<div>Авторизация</div>} />
+          <Route path="/register" element={<div>Регистрация</div>} />
+          <Route path="/users" element={<div>Пользователи</div>} />
+          <Route path="/post" element={<div>Новая статья</div>} />
+          <Route path="/post/:postId" element={<div>Статья</div>} />
+          <Route path="*" element={<div>Ошибка</div>} />
+          <Route path="/" element={<div>Главная</div>} />
+        </Routes>
+      </Content>
+      <Footer />
+    </>
+  );
+}
+
+export default Blog;
